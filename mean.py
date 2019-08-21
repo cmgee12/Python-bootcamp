aa270f3f5f42415ae45621190f2fce82cd060702
Carolyn Gee <carolyn.mei.gee@cern.ch>
2019-08-21 11:50:42 -0700

    edit message

--- mean.py
+++ mean.py
@@ -3,6 +3,6 @@ def mean(num_list):
     try:
         return sum(num_list)/len(num_list)
     except ZeroDivisionError as detail:
-        msg = ("The algebraic mean of an empty list is undefined. Please provide a list of numbers")
+        msg = ("The algebraic mean of an empty list is undefined. Please provide a list of nice numbers")
 
         raise ZeroDivisionError(detail.__str__() + "\n" + msg)
